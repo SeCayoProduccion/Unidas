2fe4e8bb8ebb80c2d362770563ef95e521509f94
KoikeDG01 <112411625+KoikeDG01@users.noreply.github.com>
2026-04-17 11:24:47 -0600

    Consultacsv

--- Consulta.py
+++ Consulta.py
@@ -1,17 +1,4 @@
-import sqlite3
 import pandas as pd
-conn = sqlite3.connect("desaparecidos.db")
-
-query = """
-SELECT entidad_desaparicion, COUNT(*) as total
-FROM casos
-WHERE fecha_desaparicion IS NOT NULL
-AND sexo = 'MUJER'
-GROUP BY entidad_desaparicion
-"""
-
-df_combo = pd.read_sql(query, conn)
-print(df_combo)
-
-
-conn.close()
+df = pd.read_csv("./RNPDNO-22-08-2023-limpio.csv", encoding="latin-1")
+consulta = df[df["Sexo"] == "MUJER"].groupby("Entidad de desaparición").size()
+print(consulta)
