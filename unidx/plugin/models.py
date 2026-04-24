cd92be24486cd3d316c41f11c3209d4c3d88ba02
KoikeDG01 <112411625+KoikeDG01@users.noreply.github.com>
2026-04-24 13:08:35 -0600

    cambios

--- unidx/plugin/models.py
+++ unidx/plugin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Flux(models.Model):
+    pub_datetime = models.DateTimeField('datetime')
+    flux = models.DecimalField(default=0.0)
