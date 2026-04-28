6e6b2cf0b92bfb0f83fd520912d4ab9cdc695c18
RindoPC <ctrl57m@comunidad.unam.mx>
2026-04-28 12:51:48 -0600

    Views.py tomado del proyecto por Victor.

--- unidx/plugin/views.py
+++ unidx/plugin/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Flux
+from datetime import datetime
 
 # Create your views here.
+
+def index(request):
+    data = Flux.objects.values()
+
+    now = datetime.now()
+    flux = Flux(now, 1.0, "hello")
+    flux.save()
+
+    return HttpResponse(data, content_type='application/json')
