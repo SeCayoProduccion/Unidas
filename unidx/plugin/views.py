bd7ae070f5992bc035be60883965ba9fdb49a0e1
indracode7 <indracode7@gmail.com>
2026-04-30 11:17:38 -0600

    views with Form code

--- unidx/plugin/views.py
+++ unidx/plugin/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Flux
 from datetime import datetime
+#Imports del form
+from django.views.generic import View
+from .forms import MyUserForm
 
 # Create your views here.
-
 def index(request):
     data = Flux.objects.values()
 
@@ -13,3 +15,12 @@ def index(request):
     flux.save()
 
     return HttpResponse(data, content_type='application/json')
+
+#parte del form
+
+def MyUserView(View):
+	form_class = MyUserForm
+	template_name = 'books/api.html'
+
+
+	return HttpResponse(data, content_type = 'application/json')
