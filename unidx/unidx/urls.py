93adf6c2a2d8237f7306f6fbc6b69d56bb12fe8e
indracode7 <indracode7@gmail.com>
2026-05-27 23:36:38 -0600

    Add core - homepage

--- unidx/unidx/urls.py
+++ unidx/unidx/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('plugin.urls')), 
+    path('', include('plugin.urls')),
+    path('core/', include('core.urls')),
 ]
